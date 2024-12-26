d03ff3c52f0b69d5dc700464d9424cbd76902e86
notmandarin <130363467+notmandarin@users.noreply.github.com>
2024-12-26 20:44:38 +0900

    [Bronze III] Title: 알람 시계, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -0,0 +1,19 @@
+H,M = map(int, input().split())
+
+if M < 45 :
+    H -= 1
+    M = M + 60 - 45
+    if M == 60 :
+        H += 1 
+        M = 0
+
+elif M == 45 :
+    M = 0
+
+else : #M > 45
+    M -= 45
+    
+if H < 0 :
+    H = 24 + H
+    
+print(H,M)
