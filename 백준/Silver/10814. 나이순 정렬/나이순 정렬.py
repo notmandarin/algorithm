cde29b930a909412ebe4981e258a9bf2eba416af
notmandarin <130363467+notmandarin@users.noreply.github.com>
2024-12-30 16:27:13 +0900

    [Silver V] Title: 나이순 정렬, Time: 2712 ms, Memory: 46020 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,13 +1,13 @@
 N = int(input())
 
 LIST = []
-for idx in range(N):
+for _ in range(N):
     age, name = input().split()
-    LIST.append((int(age), idx, name))  # 나이, 가입 순서, 이름 저장
+    LIST.append((int(age), name))  # 나이와 이름만 저장
 
-# 나이를 우선 기준으로 정렬, 가입 순서를 보조 기준으로 정렬
-sorted_LIST = sorted(LIST, key=lambda x: (x[0], x[1]))
+# 나이를 기준으로 정렬 (가입 순서는 유지됨)
+LIST.sort(key=lambda x: x[0])
 
 # 정렬 결과 출력
-for age, _, name in sorted_LIST:
+for age, name in LIST:
     print(age, name)
