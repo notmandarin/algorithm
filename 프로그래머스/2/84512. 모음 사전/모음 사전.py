2cb1abf7b4217d4d3de408eba28820410f008e37
notmandarin <130363467+notmandarin@users.noreply.github.com>
2025-01-10 20:00:58 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,12 +1,10 @@
-from itertools import product
-
 def solution(word):
-    aeiou = ['A', 'E', 'I', 'O', 'U']
-
-    dictionary = []
-    for i in range(1, 6):
-        dictionary.extend([''.join(p) for p in product(aeiou, repeat=i)])
+    vowels = ['A', 'E', 'I', 'O', 'U']
+    place_values = [781, 156, 31, 6, 1]  # 각 자리수의 기여도 미리 계산
+    result = 0
     
-    dictionary.sort()
+    for i, char in enumerate(word):
+        index = vowels.index(char)  # 해당 문자의 인덱스 (0 ~ 4)
+        result += index * place_values[i] + 1  # 해당 자리의 기여도 계산
     
-    return dictionary.index(word) + 1
+    return result
