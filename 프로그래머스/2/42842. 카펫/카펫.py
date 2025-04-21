3611b4a90d2543f90c50ba7e7bdfabff1b25e70e
notmandarin <130363467+notmandarin@users.noreply.github.com>
2025-04-21 14:29:34 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,19 +1,10 @@
-from math import sqrt
-
-def GCD(n):
-    gcd_list = []
-    for i in range(1, int(sqrt(n)) + 1):
-        if n % i == 0:
-            gcd_list.append([i, n // i])
-    return gcd_list
+import math
 
 def solution(brown, yellow):
-    gcd_sum = GCD(brown+yellow)
-    gcd_yellow = GCD(yellow)
-
-
-    for y in gcd_yellow:
-        for s in gcd_sum:
-            if (y[0] +2 == s[0]) and (y[1] +2 == s[1]):
-                answer = [s[1], s[0]]
-                return answer
+    all = brown + yellow
+    
+    for i in range(1,int(math.sqrt(all))+1) :
+        if all % i == 0 :
+            if i*2 + (all//i)*2 -4 == brown :
+                return [all//i, i]
+                break
